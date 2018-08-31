1ee931a855c5ad89380e5b880c69778674a9cbee
potrik98 <potrik98@gmail.com>
2018-08-31 08:50:45 +0200

    Complete exercise 1

--- ex1code/src/Lib.hs
+++ ex1code/src/Lib.hs
@@ -24,7 +24,7 @@ import Prelude hiding (take)
 -- finish the function "add" that takes two integers
 -- and returns the sum of them
 add :: Int -> Int -> Int
-add n m = undefined
+add n m = n + m
 
 -- complete the function "isVowel" which
 -- takes a character and returns True
@@ -32,7 +32,7 @@ add n m = undefined
 -- hint: a string is a list
 -- hint2: use `elem` from Prelude
 isVowel :: Char -> Bool
-isVowel chr = undefined
+isVowel chr = chr `elem` "aeuioyAEIOUY"
 
 data Status = One | Two | Three | None deriving (Show, Eq)
 
@@ -42,7 +42,12 @@ data Status = One | Two | Three | None deriving (Show, Eq)
 -- and returns a tuple of a Status (look above, describes the amount of subjects in the list)
 -- and the name
 amountOf :: String -> [a] -> (Status, String)
-amountOf = undefined
+amountOf string list = let n = length list in
+    (let status | n == 0 = None
+                | n == 1 = One
+                | n == 2 = Two
+                | otherwise = Three
+    in (status, string))
 
 -- TASK 2
 -- Recursion
@@ -52,7 +57,9 @@ amountOf = undefined
 -- assuming that 0th = 0 and 1st = 1
 -- do not optimize it
 fib :: Int -> Int
-fib = undefined
+fib 0 = 0
+fib 1 = 1
+fib n = (fib (n-1)) + (fib (n-2))
 
 -- TASK 3
 -- Working with lists
@@ -61,18 +68,21 @@ fib = undefined
 -- strings and returns them with the ending -ing
 -- if the string is empty, remove it from the list
 ending :: [String] -> [String]
-ending = undefined
+ending list = map (\s -> s ++ "ing") (filter (not . null) list)
 
 -- complete the function "takeInt" that
 -- an integer n and a list of integers and
 -- returns the first n elements of 
 -- the list
 takeInt :: Int -> [Int] -> [Int]
-takeInt = undefined
+takeInt _ [] = []
+takeInt n (x:xs)
+    | n < 1 = []
+    | otherwise = x : takeInt (n-1) xs
 
 -- implement "fizzbuzz" as described in exercise 1
 fizzbuzz :: [String]
-fizzbuzz = undefined
+fizzbuzz = map (\a -> let s = concat [if a `mod` fst t == 0 then snd t else "" | t <- [(3, "Fizz"), (5, "Buzz")]] in if null s then show a else s) [1..100]
 
 printFizz :: IO ()
 printFizz = mapM_ putStrLn fizzbuzz
@@ -84,18 +94,18 @@ printFizz = mapM_ putStrLn fizzbuzz
 -- even numbers that are equal or greater than 0
 -- use a list comprehension
 listOfEven :: [Integer]
-listOfEven = undefined
+listOfEven = [n | n <- [0..], even n]
 
 -- create a list of tuples, "zipped"
 -- where each tuple contains the nth entry
 -- in the lists [1..26] and ['a'..'z']
 -- hint: parallel list comprehension
 zipped :: [(Int, Char)]
-zipped = undefined
+zipped = zip [1..26] ['a'..'z']
 
 -- create a list that contains the cartesian
 -- product of the two vectors [4, 6, 8]
 -- and [3, 7, 9]
 -- use a list comprehension
 cartesian :: [(Int, Int)]
-cartesian = undefined
+cartesian = [(a, b) | a <- [4,6,8], b <- [3,7,9]]
