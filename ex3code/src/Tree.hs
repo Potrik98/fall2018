e076e78400e3a2b064618e03262241b55f8d5f02
potrik98 <potrik98@gmail.com>
2018-09-13 00:50:38 +0200

    Tree.hs

--- ex3code/src/Tree.hs
+++ ex3code/src/Tree.hs
@@ -11,14 +11,15 @@ data Tree a = Branch (Tree a) a (Tree a) | Leaf a
 -- The Foldable instance might prove tricky to define, so
 -- defining the specific functions first may be easier!
 treeSum :: (Num a) => Tree a -> a
-treeSum = undefined
+treeSum = foldr (+) 0
 
 treeConcat :: Tree String -> String
-treeConcat = undefined
+treeConcat = foldr (++) []
 
 treeMaximum :: (Ord a) => Tree a -> a
 treeMaximum = undefined
 
 -- Write a Foldable instance for Tree.
 instance Foldable Tree where
-    foldr = undefined
+    foldr f b (Leaf a) = f a b
+    foldr f b (Branch u a v) = foldr f (foldr f (f a b) u) v
