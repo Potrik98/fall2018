5cca555b803e34472d8be189ed087a048117c9d2
Anders <andershopl@hotmail.com>
2018-10-25 15:20:07 +0200

    Adds dependencies and some code for account and bank

--- scala_project/task2-project/src/main/scala/Account.scala
+++ scala_project/task2-project/src/main/scala/Account.scala
@@ -24,21 +24,36 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
 
     def getTransactions: List[Transaction] = {
         // Should return a list of all Transaction-objects stored in transactions
-        ???
+        var list = List[Transaction]()
+        for ((k,v) <- transactions) {
+            list ::= v
+        }
+        list
     }
 
     def allTransactionsCompleted: Boolean = {
         // Should return whether all Transaction-objects in transactions are completed
-        ???
+
+        for ((k,v) <- transactions) {
+            if (!v.isCompleted) {
+                return false
+            }
+        }
+        return true
+        
     }
 
-    def withdraw(amount: Double): Unit = ??? // Like in part 2
-    def deposit(amount: Double): Unit = ??? // Like in part 2
-    def getBalanceAmount: Double = ??? // Like in part 2
+    def withdraw(amount: Double): Unit = {
+        if (amount < balance.amount) {
+            balance.amount -= amount // Like in part 2
+        }
+    }
+    def deposit(amount: Double): Unit = balance.amount += amount // Like in part 2
+    def getBalanceAmount: Double = return balance.amount // Like in part 2
 
     def sendTransactionToBank(t: Transaction): Unit = {
         // Should send a message containing t to the bank of this account
-        ???
+        
     }
 
     def transferTo(accountNumber: String, amount: Double): Transaction = {
@@ -76,11 +91,11 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
 			???
 		}
 
-		case BalanceRequest => ??? // Should return current balance
+		case BalanceRequest => balance.amount // Should return current balance
 
 		case t: Transaction => {
 			// Handle incoming transaction
-			???
+			balance.amount += t.amount
 		}
 
 		case msg => ???
